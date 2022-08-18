e4f29a19b73b6aabed9af4ed061c011792e270da
ahmetdogukankonuk <ahmetdogukankonuk@gmail.com>
2022-08-18 17:43:41 +0300

    Footer section created

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import './footer.css';
+import {BsFacebook} from 'react-icons/bs';
+import {BsInstagram} from 'react-icons/bs';
+import {BsTwitter} from 'react-icons/bs';
 
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer id='footer'>
+      <a href="#" className='footer__logo'>
+        ADK
+      </a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="facebook.com"><BsFacebook/></a>
+        <a href="instagram.com"><BsInstagram/></a>
+        <a href="twitter.com"><BsTwitter/></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; ADK LLC. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
